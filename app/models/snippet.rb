0720ccdf801f526786b3fa2648cb77122f1157c9
Yuri Omelchuk <jurgen@upscript.com>
2012-06-03 17:31:41 +0300

    updated devise config

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,7 +1,7 @@
 class Snippet < ActiveRecord::Base
   belongs_to :user
 
-  validates :name, :body, presence: true
+  validates :name, :body, :board_id, presence: true
 
   attr_accessible :name, :body, :board_id
 end
